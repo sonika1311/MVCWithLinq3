438436e16ed81818dc94c871a119a83a3fc036e4
sonika1311 <sonikakumaribom@gmail.com>
2024-07-25 22:41:27 +0530

    Add DB table models

---
+++ MVCWithLinq3/Controllers/EmployeeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithLinq3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        
+    }
+}
---
+++ MVCWithLinq3/Models/EmployeeDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithLinq3.Models
+{
+    public class EmployeeDAL
+    {
+        MVCDBDataContext dc = new MVCDBDataContext(ConfigurationManager.ConnectionStrings["MVCDBConnectionString"].ConnectionString);
+
+    }
+}
